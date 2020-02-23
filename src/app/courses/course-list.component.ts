c49e5e0d32066b24c7c79ca9dc03ada0349e5b14
luiz-ortega <luizngust@hotmail.com>
2020-02-23 16:33:01 -0300

    feat: Dependencies Injection

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
   selector: "app-course-list",
@@ -8,38 +9,9 @@ import { Course } from "./course";
 export class CourseListComponent implements OnInit {
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService) {}
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: "Angular: Froms",
-        imageUrl: "/assets/images/forms.png",
-        price: 99.99,
-        code: "XPS-8796",
-        duration: 120,
-        rating: 2.5,
-        releaseDate: "December, 2, 2019"
-      },
-      {
-        id: 2,
-        name: "Angular: HTTP",
-        imageUrl: "/assets/images/http.png",
-        price: 49.99,
-        code: "XPS-2756",
-        duration: 120,
-        rating: 3.5,
-        releaseDate: "December, 2, 2019"
-      },
-      {
-        id: 3,
-        name: "Angular: HTTP",
-        imageUrl: "/assets/images/http.png",
-        price: 49.99,
-        code: "XPS-2756",
-        duration: 120,
-        rating: 4,
-        releaseDate: "December, 2, 2019"
-      }
-    ];
+    this.courses = this.courseService.retriveAll();
   }
 }
